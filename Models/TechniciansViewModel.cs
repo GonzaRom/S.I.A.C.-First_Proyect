8d617addca2e26aac50981cd2400a7ff7ff8f163
GonzaRom <rom.gonzalo.i@gmail.com>
2020-07-20 03:11:40 -0300

    add new gitignore

--- Models/TechniciansViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace S.I.A.C.Models
-{
-    public class TechniciansViewModel
-    {
-        public int keyTechnician { get; set; }
-        public string nameTechnician { get; set; }
-    }
-}
