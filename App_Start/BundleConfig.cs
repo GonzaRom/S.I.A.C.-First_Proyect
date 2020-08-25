d9b5618c5155386273e73b509797640a64417aa9
GonzaRom <rom.gonzalo.i@gmail.com>
2020-08-25 01:48:31 -0300

    Fix View, add new modal

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -16,7 +16,7 @@ namespace S.I.A.C
                 "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                "~/Scripts/bootstrap.js"));
+                "~/Scripts/bootstrap.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                 "~/Content/bootstrap.min.css",
